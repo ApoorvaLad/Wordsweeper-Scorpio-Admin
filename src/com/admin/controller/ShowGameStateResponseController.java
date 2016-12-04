cb1c0dddd712f76f8147c06b3c2a07d9f3740903
rishithakiran <rishithakiran1@gmail.com>
2016-12-04 14:04:28 -0500

    ShowGameState

--- src/com/admin/controller/ShowGameStateResponseController.java
+++ src/com/admin/controller/ShowGameStateResponseController.java
@@ -1,12 +1,20 @@
 package com.admin.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import com.admin.xml.Message;
+import com.model.Game;
 import com.model.Model;
+import com.model.Player;
 import com.view.Application;
 
 /**
  * Controller to display the current state of the game
- * @author Apoorva
+ * @author Rishitha
  *
  */
 public class ShowGameStateResponseController extends ControllerChain{
@@ -25,6 +33,24 @@ public class ShowGameStateResponseController extends ControllerChain{
 		if (!type.equals("boardResponse")) {
 			return next.process(response);
 		}
+		Node listResponse = response.contents.getFirstChild();
+		Game game = new Game();
+		ArrayList<Player> players = null;
+		NodeList list = listResponse.getChildNodes();
+		for (int i = 0; i < list.getLength(); i++) {
+			Player player=new Player();
+			Node n = list.item(i);
+			String name = n.getAttributes().getNamedItem("name").getNodeValue();
+			player.setName(name);
+			String score = n.getAttributes().getNamedItem("score").getNodeValue();
+			player.setScore(score);
+			players.add(player);
+			game.setPlayer(players);
+
+		app.getAdminPanel().getGamePanel().getModel().addElement("Name: " + player.getname());
+		app.getAdminPanel().getGamePanel().getModel().addElement("Score: " + player.getScore());
+		}
+		
 		return true;
 	}
 
